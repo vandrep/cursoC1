19005f006064b8aa7d00fa71743b0f99685211e1
F0741372 Andre Pine da Cunha <f0741372@evvdi020500229.intrabb.bb.com.br>
2019-01-16 15:48:23 -0200

    primeiras aulas

--- curso1.c
+++ curso1.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
 int main() {
-	printf("Bem vindo ao nosso jogo de adivinhação");
+	printf("******************************************\n");
+	printf("* Bem vindo ao nosso jogo de adivinhação *\n");
+	printf("******************************************\n");
+
+	int numeroSecreto = 42;
+
+	int chute;
+
+	printf("Qual é o seu chute? ");
+	scanf("%d", &chute);
+	printf("Seu chute foi %d\n", chute);
+
+	if (chute == numeroSecreto) {
+		printf("Parabéns, você acertou!\n");
+	} else {
+		if (chute > numeroSecreto) {
+			printf("O seu chute é maior que o número secreto.\n");
+		} else {
+			printf("O seu chute é menor que o número secreto.\n");
+		}
+	}
 }
---
+++ multiplicaDois.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	int num1;
+	int num2;
+	printf("Digite duas variáveis inteiras:\n");
+	scanf("%d", &num1);
+	scanf("%d", &num2);
+	printf("O resultado da multiplicação é %d", num1 * num2);
+}
