825584844aac49574fd0df3ac0353bbdb209fb09
Andre <andrepinedacunha@gmail.com>
2019-01-21 09:23:07 -0200

    Novas alterações

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <windows.h>
+#include <locale.h>
+
+#define NUMERO_DE_TENTATIVAS 5
 
 int main() {
+	UINT CPAGE_UTF8 = 65001;
+	UINT CPAGE_DEFAULT = GetConsoleOutputCP();
+	SetConsoleOutputCP(CPAGE_UTF8);
+
 	printf("******************************************\n");
 	printf("* Bem vindo ao nosso jogo de adivinhação *\n");
 	printf("******************************************\n");
@@ -9,8 +17,8 @@ int main() {
 
 	int chute;
 
-	for (int i = 1; i <= 3; i++) {
-		printf("Tentativa %d de 3\n", i);
+	for (int i = 1; i <= NUMERO_DE_TENTATIVAS; i++) {
+		printf("Tentativa %d de %d\n", i, NUMERO_DE_TENTATIVAS);
 		printf("Qual é o seu chute? ");
 		scanf("%d", &chute);
 		printf("Seu chute foi %d\n", chute);
