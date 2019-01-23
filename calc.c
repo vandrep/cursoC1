e9093ac98526f8c36ea2d606f6e15fc0ae002714
Andre Pine da Cunha <andre.cunha@bb.com.br>
2019-01-23 12:47:09 -0200

    ajuste final no trabalho

--- calc.c
+++ calc.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
+#include <windows.h>
+#include <locale.h>
 
 int main(){
-    double num1 num2;
+    SetConsoleOutputCP(65001);
+    double num1, num2;
     int operacao;
     printf("***************\n");
     printf("* Calculadora *\n");
     printf("***************\n");
     printf("Digite o primeiro número: ");
-    scanf("%f", num1);
+    scanf("%lf", &num1);
     printf("\n1 = soma, 2 = subtração, 3 = divisão, 4 = multiplicação");
     printf("Digite a operação: ");
-    scanf("%d", operacao);
+    scanf("%d", &operacao);
     printf("\nDigite o segundo número: ");
-    scanf("%f", num2);
+    scanf("%lf", &num2);
     printf("O resultado é:");
     if(operacao ==1){
-        printf("%f", num1 + num2);
+        printf("%.1f", num1 + num2);
     }
     else if(operacao == 2){
-        printf("%f", num1 - num2);
+        printf("%.1f", num1 - num2);
     }
     else if(operacao == 3){
-        printf("%f", num1 / num2);
+        printf("%.1f", num1 / num2);
     }
     else if(operacao == 4){
-        printf("%f", num1 * num2);
+        printf("%.1f", num1 * num2);
     }
     return 0;
 }
